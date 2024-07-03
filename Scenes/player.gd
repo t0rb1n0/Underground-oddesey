43aecdf208af8242372f8575451e7ad39679577f
t0rb1n0 <torbar47@kingshigh.school.nz>
2024-07-03 14:58:32 +1200

    1.2 version
    working on animating the player

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -23,15 +23,15 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+	var direction = Input.get_axis("left", "right")
 	if direction:
 		velocity.x = move_toward(velocity.x, direction * speed, acceleration)
 	else:
-		velocity.x = move_toward(velocity.x, 0, acceleration/2)
+		velocity.x = move_toward(velocity.x, 0, speed)
 
 	move_and_slide()
