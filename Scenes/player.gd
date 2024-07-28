41bff997b7d8af3061bdc6a8c51470f34ef56b9c
t0rb1n0 <torbar47@kingshigh.school.nz>
2024-07-29 08:59:27 +1200

    1.4 version
    Adding sprite and map tileset

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -16,7 +16,7 @@ var anim_state = state.IDLE
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-func update_state ():
+func update_state():
 	if anim_state == state.HURT:
 		return
 	if is_on_floor():
@@ -24,7 +24,28 @@ func update_state ():
 			anim_state = state.IDLE
 		elif velocity.x != 0:
 			anim_state = state.RUNNING
+	else:
+		if velocity.y < 0:
+			anim_state = state.JUMPUP
+		else:
+			anim_state = state.JUMPDOWN
 
+func update_animation(direction):
+	if direction > 0:
+		animator.flip_h = false
+	elif direction < 0:
+		animator.flip_h = true
+	match anim_state:
+		state.IDLE:
+			animation_player.play("idle")
+		state.RUNNING:
+			animation_player.play("run")
+		state.JUMPUP:
+			animation_player.play("jump_up")
+		state.JUMPDOWN:
+			animation_player.play("jump_down")
+		state.HURT:
+			animation_player.play("hurt")
 
 func _physics_process(delta):
 	# Add the gravity.
@@ -42,5 +63,7 @@ func _physics_process(delta):
 		velocity.x = move_toward(velocity.x, direction * speed, acceleration)
 	else:
 		velocity.x = move_toward(velocity.x, 0, speed)
-
+		
+	update_state()
+	update_animation(direction)
 	move_and_slide()
